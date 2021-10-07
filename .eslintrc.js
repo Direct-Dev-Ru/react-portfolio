e9c70b0897ab110d6f9ce7fee0452abb1e9c54f8
Direct-Dev-Ru <info@direct-dev.ru>
2021-10-07 08:35:31 +0000

    starter code added

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    env: {
+      es6: true,
+      browser: true,
+      node: true,
+    },
+    extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+    // extends: ['react-app', 'plugin:jest/recommended', 'jest-enzyme'],
+    plugins: ['babel', 'import', 'jsx-a11y', 'react', 'prettier'],
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      'linebreak-style': 'off', // Неправильно работает в Windows.
+      'react/prop-types': 'off',
+      'arrow-parens': 'off', // Несовместимо с prettier
+      'object-curly-newline': 'off', // Несовместимо с prettier
+      'no-mixed-operators': 'off', // Несовместимо с prettier
+      'arrow-body-style': 'off', // Это - не наш стиль?
+      'function-paren-newline': 'off', // Несовместимо с prettier
+      'no-plusplus': 'off',
+      'space-before-function-paren': 0, // Несовместимо с prettier
+      'max-len': ['error', 100, 2, { ignoreUrls: true }],
+      'no-console': 'off', // airbnb использует предупреждение
+      'no-unused-vars': 'off', // airbnb использует предупреждение
+      'no-alert': 'error', // airbnb использует предупреждение
+      'no-param-reassign': 'off', // Это - не наш стиль?
+      radix: 'off', // parseInt, parseFloat и radix выключены. Мне это не нравится.
+  
+      'react/require-default-props': 'off', // airbnb использует уведомление об ошибке
+      'react/forbid-prop-types': 'off', // airbnb использует уведомление об ошибке
+      'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  
+      'prefer-destructuring': 'off',
+      'react/state-in-constructor': 'off',
+      'react/no-find-dom-node': 'off', // Я этого не знаю
+      'react/no-did-mount-set-state': 'off',
+      'react/no-unused-prop-types': 'off', // Это всё ещё работает нестабильно
+      'react/jsx-one-expression-per-line': 'off',
+      'import/no-unresolved': 'off',
+  
+      'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+      'jsx-a11y/label-has-for': [
+        2,
+        {
+          required: {
+            every: ['id'],
+          },
+        },
+      ], // для ошибки вложенных свойств htmlFor элементов label
+  
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+// eslint-disable-next-line
+import React from 'react';
 
 export default function App() {
-  return "App";
+  return 'App';
 }
